6517642ddb3bc9863e8a6613813592179bf4ae15
Pavan Gollapalli <pavankgollapalli@gmail.com>
2024-03-11 13:00:03 -0700

    basic pkce flow working, access token in local storage

--- src/AuthCallback.tsx
+++ src/AuthCallback.tsx
@@ -1,7 +1,58 @@
+import { useEffect } from 'react';
 import './App.css'
+import { useNavigate } from 'react-router-dom';
 
 const AuthCallback = () => {
-  // handle callback, set access token in session storage
+  let navigate = useNavigate();
+  const urlParams = new URLSearchParams(window.location.search);
+  const code = urlParams.get('code');
+  console.log("initialized callback")
+  if(!code) {
+    // TODO better error handling here?
+    navigate('/')
+    return;
+  }
+
+  const clientId = import.meta.env.YOODLI_SPOTIFY_CLIENT_ID;
+  const redirectUri = import.meta.env.YOODLI_SPOTIFY_REDIRECT_URI;
+  const tokenUrl = new URL("https://accounts.spotify.com/api/token")
+
+  const getToken = async (code: string) => {
+    let codeVerifier = localStorage.getItem('code_verifier');
+
+    const payload = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams({
+        client_id: clientId,
+        grant_type: 'authorization_code',
+        code,
+        redirect_uri: redirectUri,
+        code_verifier: codeVerifier,
+      } as any),
+    }
+
+    console.log("payload", payload)
+
+    const body = await fetch(tokenUrl, payload);
+    const response = await body.json();
+    console.log(response)
+    // TODO need error checking here
+
+    localStorage.setItem('access_token', response.access_token);
+  }
+
+  useEffect(() => {
+    console.log("before", code)
+    getToken(code).then(() => {
+      console.log("after processing", localStorage)
+      navigate('/search')
+    }).catch((e) => {
+      console.error(e)
+    })
+  }, [code])
 
   return (
     <>
--- src/AuthStart.tsx
+++ src/AuthStart.tsx
@@ -1,10 +1,65 @@
+import { useEffect } from 'react';
+
 import './App.css'
 
-// TODO: document why this is a component
+// TODO: document why this is a component and not run in root
 const AuthStart = () => {
   // run pkce flow here
+  const clientId = import.meta.env.YOODLI_SPOTIFY_CLIENT_ID
 
-  // redirect to spotify auth link
+  const generateRandomString = (length: number) => {
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const values = crypto.getRandomValues(new Uint8Array(length));
+    return values.reduce((acc, x) => acc + possible[x % possible.length], "");
+  }
+
+  const codeVerifier  = generateRandomString(64);
+
+  const sha256 = async (plain: string) => {
+    const encoder = new TextEncoder()
+    const data = encoder.encode(plain)
+    return window.crypto.subtle.digest('SHA-256', data)
+  }
+
+  const base64encode = (input: ArrayBuffer) => {
+    return btoa(String.fromCharCode(...new Uint8Array(input)))
+      .replace(/=/g, '')
+      .replace(/\+/g, '-')
+      .replace(/\//g, '_');
+  }
+
+  const initializeAuth = async () => {
+    const hashed = await sha256(codeVerifier)
+    const codeChallenge = base64encode(hashed);
+
+    // TODO is this the right scope?
+    const scope = 'user-library-read';
+    const authUrl = new URL("https://accounts.spotify.com/authorize")
+
+    window.localStorage.setItem('code_verifier', codeVerifier);
+    const redirectUri = 'http://localhost:5173/callback';
+
+    const params =  {
+      response_type: 'code',
+      client_id: clientId,
+      scope,
+      code_challenge_method: 'S256',
+      code_challenge: codeChallenge,
+      redirect_uri: redirectUri,
+    }
+    console.log(params)
+
+    authUrl.search = new URLSearchParams(params).toString();
+    window.location.href = authUrl.toString();
+  }
+
+  useEffect(() => {
+    initializeAuth().then(() => {
+      // this will never run, as the flow will redirect to /callback
+    }).catch(e => {
+      console.error(e)
+    })
+  }, [])
 
   return (
     <>
